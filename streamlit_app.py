7e002ca47487a082c59fed9f851dcfdd3f4f776f
Shubh Patel <104150829+Shubhgpatel@users.noreply.github.com>
2025-04-22 16:01:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with st.expander('Data Visualtization'):
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  bill_lenght_mm = st.slider('Bill lenght (mm)', 32.1, 59.6, 43.9)
+  bill_length_mm = st.slider('Bill lenght (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('Flipper lenght (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
