35ae45ebdf09782bda8c7a489fb18832f8416268
Shubh Patel <104150829+Shubhgpatel@users.noreply.github.com>
2025-04-22 15:28:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.expander('Data'):
 with st.expander('Data Visualtization'):
   st.scatter_chart(data=df , x='bill_length_mm', y='body_mass_g', color = 'species')
 
-# Data Preparations
+# Input Features
 with st.sidebar:
   st.header('Input features')
   island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
@@ -42,6 +42,13 @@ with st.sidebar:
   input_df = pd.DataFrame(data , index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
+with st.expander('Input Features'):
+  st.write('**Input Penguin**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins 
+
+# Data Preparations
 # Encode X
 encode = ['island','sex']
 df_penguins = pd.get_dummies(input_penguins,prefix = encode)
@@ -56,16 +63,12 @@ def target_encode(val):
   return target_mapper[val]
 
 y = y_raw.apply(target_encode)
-y
-y_raw
 
-with st.expander('Input Features'):
-  st.write('**Input Penguin**')
-  input_df
-  st.write('**Combined penguins data**')
-  input_penguins  
-  st.write('**Encoded input penguin**')
+with st.expander('Data perparation'):
+  st.write('**Encoded X (input penguin)**')
   input_row
+  st.write('**Encoded y**')
+  y
 
   
 
