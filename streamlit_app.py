e74366b85f334714c0a57a4e81ce7159840d0e94
Shubh Patel <104150829+Shubhgpatel@users.noreply.github.com>
2025-04-22 16:03:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,23 +25,25 @@ with st.expander('Data Visualtization'):
 # Input Features
 with st.sidebar:
   st.header('Input features')
-  island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-  bill_length_mm = st.slider('Bill lenght (mm)', 32.1, 59.6, 43.9)
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('Flipper lenght (mm)', 172.0, 231.0, 201.0)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-  gender = st.selectbox('Gender',('male','female'))
+  gender = st.selectbox('Gender', ('male', 'female'))
 
-# Create a Dataframe for the input Features.
- data = {'island': island,
-        'bill_length_mm': bill_length_mm,
-        'bill_depth_mm': bill_depth_mm,
-        'flipper_length_mm': flipper_length_mm,
-        'body_mass_g': body_mass_g,
-        'sex': gender}
-  
-  input_df = pd.DataFrame(data , index=[0])
+  # Create a Dataframe for the input Features
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+
+  input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+
 
 with st.expander('Input Features'):
   st.write('**Input Penguin**')
