d69eebbd00fd75695c23963b6e1d9f0c0ff73de9
Shubh Patel <104150829+Shubhgpatel@users.noreply.github.com>
2025-04-22 15:16:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ target_mapper = {'Adelie' : 0,
                 'Chinstrap' : 1,
                 'Gentoo' : 2}
 
-def target_encode(val)
+def target_encode(val):
   return target_mapper[val]
 
 with st.expander('Input Features'):
