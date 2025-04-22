93940354dff540e81e93cc026f8ade0df498ddac
Shubh Patel <104150829+Shubhgpatel@users.noreply.github.com>
2025-04-22 16:05:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,6 +82,6 @@ clf.fit(X, y)
 
 # Apply Model to make predictions
 prediction =  clf.predict(input_row)
-prediction_proba = clf.prerdict_proba(input_row)
+prediction_proba = clf.predict_proba(input_row)
 prediction_proba
 
